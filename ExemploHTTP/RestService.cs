c1a58ffe2103baf000ee43e3291dcf7c63044422
Diego Matheus <dms.diego.2019@gmail.com>
2024-06-03 22:52:45 -0300

    added  .gitignore

---
+++ ExemploHTTP/RestService.cs
@@ -0,0 +1,55 @@
+﻿using ExemploHTTP.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ExemploHTTP
+{
+    public class RestService
+    {
+        HttpClient _client;
+        JsonSerializerOptions _serializerOptions;
+
+        public ObservableCollection<Postagem> Postagens { get; set; }
+
+        public RestService()
+        {
+            Postagens = null;
+            _client = new HttpClient(); //instanciando
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase, //primeira letra minuscula (CamelCase)
+                WriteIndented = true //identação
+            };
+        }
+        public async Task<ObservableCollection<Postagem>> GetPostagemsAsync() //Faz o get para visualizar
+        {
+            Postagens = new ObservableCollection<Postagem>(); // ira fazer nova lista de postagem, pega o que tem na model Postagem
+
+            Uri uri = new Uri("https://jsonplaceholder.typicode.com/posts");
+            //tentar buscar a informcao da Uri /\
+
+
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(uri); //pega resposta do uri
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync(); //pega conteudo e le como string
+                    Postagens = JsonSerializer.Deserialize<ObservableCollection<Postagem>>(content, _serializerOptions);
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            return Postagens ??[];
+        }
+     }
+}
---
+++ ExemploHTTP/ViewModel/PostagemViewModel.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ExemploHTTP.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ExemploHTTP.ViewModel
+{
+    public partial class PostagemViewModel : ObservableObject
+    {
+        private readonly RestService _restService;
+
+        public PostagemViewModel() // instanciando o RestService
+        {
+            _restService = new RestService();
+            GetPostagensAsyncCommand = new Command(async () => GetPostagemsAsync());
+        }
+
+        [ObservableProperty]
+        private int _userId;
+
+        [ObservableProperty]
+        private int _id;
+
+        [ObservableProperty]
+        private string _title;
+
+        [ObservableProperty]
+        private string _body;
+
+
+        public ICommand GetPostagensAsyncCommand { get; }
+
+        public ObservableCollection<Postagem> _postagens = new ObservableCollection<Postagem>(); // para fazer colecao
+        public ObservableCollection<Postagem> Postagens
+        {
+            get { return _postagens; }
+            set { _postagens = value; }
+        }
+
+        public async Task  GetPostagemsAsync()
+        {
+            Postagens = await _restService.GetPostagemsAsync();
+        }
+    }
+}
